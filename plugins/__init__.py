f7a420247fb7326eb9f099f93cb5041799c83916
Abdullah <abdullah@refreshworks.nl>
2025-12-05 15:50:18 +0100

    remove plugins for now and focus on the core app

--- plugins/__init__.py
+++
@@ -1,1 +0,0 @@
-"""FastCMS Plugins."""
--- plugins/langflow/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Langflow integration plugin for FastCMS.
-
-Provides visual AI workflow building through Langflow integration.
-"""
-
-from plugins.langflow.plugin import LangflowPlugin
-
-__all__ = ["LangflowPlugin"]
